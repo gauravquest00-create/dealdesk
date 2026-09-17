a2809b38798ad7d770e92906db5c10e60f833003
BlueBuck <gauravquest00@gmail.com>
2026-09-17 14:25:02 +0530

    Update App.jsx

--- superadmin/src/App.jsx
+++ superadmin/src/App.jsx
@@ -11,24 +11,26 @@ import { AuditLogsPage } from './pages/AuditLogsPage.jsx';
 import { SuperAdminProfile } from './pages/SuperAdminProfile.jsx';
 import { CreateBusinessPage } from './pages/CreateBusinessPage.jsx';
 import { PaymentsPage } from './pages/PaymentsPage.jsx';
+import { ResetBusinessPassword } from './pages/ResetBusinessPassword.jsx';
 
 export const App = () => {
   return (
     <Routes>
       <Route path="/admin/login" element={<SuperAdminLogin />} />
+
       <Route path="/admin" element={<SuperAdminLayout />}>
         <Route index element={<SuperAdminDashboard />} />
         <Route path="businesses" element={<BusinessesPage />} />
+        <Route path="businesses/new" element={<CreateBusinessPage />} />
         <Route path="businesses/:id" element={<BusinessDetailPage />} />
+        <Route path="businesses/reset-password" element={<ResetBusinessPassword />} />
         <Route path="plans" element={<PlansPage />} />
         <Route path="subscriptions" element={<SubscriptionsPage />} />
-        <Route path="payments" element={<SubscriptionsPage />} />
+        <Route path="payments" element={<PaymentsPage />} />
         <Route path="audit-logs" element={<AuditLogsPage />} />
         <Route path="profile" element={<SuperAdminProfile />} />
-        <Route path="/admin/businesses/new" element={<CreateBusinessPage />} />
-<Route path="/admin/payments" element={<PaymentsPage />} />
-
       </Route>
+
       <Route path="*" element={<Navigate to="/admin" replace />} />
     </Routes>
   );
